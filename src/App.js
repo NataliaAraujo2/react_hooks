08fd4d8be3a86e6e15207489d037525d4684cb13
NataliaAraujo2 <nataliaanog@gmail.com>
2023-05-17 15:38:19 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+//Pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+//Router
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Ract Hooks</h1>
+      <BrowserRouter>
+      <ul>
+        <li>
+<Link to="/">HOME</Link>
+        </li>
+        <li>
+<Link to="/about">ABOUT</Link>
+        </li>
+          </ul>
+          <Routes>
+            <Route path="/" element={<Home userName={"Natália"} />} />
+            <Route path="/about" element={<About />} />          
+           
+          </Routes>
+
+          </BrowserRouter>
+          </div>
   );
 }
 
